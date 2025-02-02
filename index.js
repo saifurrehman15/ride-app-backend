98a859b97bb2c5caeea8bbcd274353933d4bb2d4
saifurrehman15 <saifkhandada72@gmail.com>
2025-02-02 06:26:44 +0500

    add

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 import express from "express";
 import "dotenv/config";
+import mongoose from "mongoose";
+import cors from "cors";
+// ---- Importing-Routes //
+import  loginAuth  from "./auth/login.js";
+import  signUp  from "./auth/signup.js";
+// ----- //
 const app = express();
 const PORT = process.env.PORT;
-console.log(PORT);
-(function name(params) {
-    
-})
+
+// ---- Others ---- //
+app.use(cors())
+app.use(express.json());
+  // ----- //
+
+  // ---- Mongodb-Connection ---- //
+  (async function connectDb() {
+    try {
+      if (mongoose.connection.readyState !== 1) {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MONGODB connected");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+)();
+// ----- //
+
+// ---- Public-Routes ---- //
 app.get("/", (req, res) => {
   res.send("Severs is running on " + PORT);
 });
 
+// ----- //
+
+// ---- Private-Routes ---- //
+
+// ----- //
+
+// ---- Auth-Routes ---- //
+app.use("/auth/login", loginAuth);
+// ----- //
+
+// ---- User-Routes ---- //
+
+// ----- //
+
+// ---- Server-Listening / Server Running ---- //
 app.listen(PORT, () => console.log("Sever is running on " + PORT));
+// ----- //
