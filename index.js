fa5ece9ef64bf36b8579aa1f40a2f7c0d769065d
saifurrehman15 <saifkhandada72@gmail.com>
2025-02-02 05:57:47 +0500

    update

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT;
 console.log(PORT);
-
+(function name(params) {
+    
+})
 app.get("/", (req, res) => {
-  res.send("Sever is running on " + PORT);
+  res.send("Severs is running on " + PORT);
 });
 
 app.listen(PORT, () => console.log("Sever is running on " + PORT));
